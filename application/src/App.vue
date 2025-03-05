ce503caa5ffda5d9a545b43265f6481e6a840ae3
taryn-bilsborough_msmg <taryn.bilsborough@moneysupermarket.com>
2025-03-05 19:46:35 +0000

    vue app set-up and config

---
+++ application/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  background-color: #F4AFAB;
+}
+
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  flex-direction: column;
+}
+</style>
