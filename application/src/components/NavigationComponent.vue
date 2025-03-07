2d8d188db78777e7365973cb24a5173fdc217933
tarynJade <167077142+tarynJade@users.noreply.github.com>
2025-03-07 17:31:20 +0000

    edited navigation routes for choose for me

--- application/src/components/NavigationComponent.vue
+++ application/src/components/NavigationComponent.vue
@@ -2,8 +2,8 @@
     <nav>
       <ul>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/breeds">Cat Breeds</RouterLink>
-        <RouterLink to="/breed-of-the-day">Cat Breed Of The Day</RouterLink>
+        <RouterLink to="/cats">Cats</RouterLink>
+        <RouterLink to="/choose-for-me">Choose For Me!</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
       </ul>
     </nav>
